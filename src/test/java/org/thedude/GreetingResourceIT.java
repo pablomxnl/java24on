b704f0c76f3bb8772971a1af68d83984f427472f
Pablo Gutiérrez <1665721+pablomxnl@users.noreply.github.com>
2025-04-01 11:30:42 +0200

    Initial commit

---
+++ src/test/java/org/thedude/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.thedude;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
